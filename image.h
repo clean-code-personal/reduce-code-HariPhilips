e3996b0b8444e95a73830e2802a28194beb56647
V R, Harikrishnan <harikrishnan.vr@philips.com>
2024-04-30 22:45:46 +0530

    build error

--- image.h
+++ image.h
@@ -11,16 +11,17 @@ public:
 	Image(uint16_t rows, uint16_t columns): m_rows(rows), m_columns(columns) {
 		m_pixels = new uint8_t[m_rows * m_columns];
 	}
+
 	~Image() {
 		std::cout << "freeing pixel memory.\n";
 		delete[] m_pixels;
 	}
+
 	bool isSizeValid() {
 		return m_rows <= 1024 && m_columns <= 1024;
 	}
 
 	void pixelRunner(PixelProcessCallback pixelProcessor) {
-		std::cout << "-- pixelRunner: lambda version\n";
 		for (int x = 0; x < m_rows; x++) {
 			for (int y = 0; y < m_columns; y++) {
 				int pixelIndex = x * m_columns + y;
@@ -30,7 +31,6 @@ public:
 	}
 
 	void pixelRunner(PixelProcessCallback2 pixelProcessor, std::shared_ptr<Image> imageToAdd) {
-		std::cout << "-- pixelRunner: lambda version\n";
 		for (int x = 0; x < m_rows; x++) {
 			for (int y = 0; y < m_columns; y++) {
 				int pixelIndex = x * m_columns + y;
@@ -39,7 +39,7 @@ public:
 		}
 	}
 
-private:
+
 	const uint16_t m_rows;
 	const uint16_t m_columns;
 	uint8_t* m_pixels; // max 1k x 1k image
